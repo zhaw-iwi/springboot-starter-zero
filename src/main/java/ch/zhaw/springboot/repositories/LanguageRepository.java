6898acf43b70c96c3b1cb4e9536fefdca999d16b
Jan Jörg <jan.joerg.jj@gmail.com>
2022-12-19 12:52:44 +0100

    test

--- src/main/java/ch/zhaw/springboot/repositories/LanguageRepository.java
+++ src/main/java/ch/zhaw/springboot/repositories/LanguageRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface LanguageRepository extends JpaRepository<Language, Long> {
-  // test
+  
 
   @Query("SELECT la FROM Language la WHERE la.origin = ?1")
   public List<Language> findLanguagesByOrigin(String origin);
