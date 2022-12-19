2708c024b260e0b5d5d0e4f6ed9d6ff66060206d
imiwin <105289489+imiwin@users.noreply.github.com>
2022-12-19 09:11:47 +0100

    test

--- src/main/java/ch/zhaw/springboot/entities/Person.java
+++ src/main/java/ch/zhaw/springboot/entities/Person.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+//HOMO
 @Entity
 public class Person {
 
